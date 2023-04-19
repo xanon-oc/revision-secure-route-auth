45b4f06bf58f56f63bef21469c8a7ee71ba7d115
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-04-19 13:49:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>
+        Hello <Button variant="danger">Hello</Button>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
